cfa58287aeb670573e122488bef982de10e907bf
zavernutiy <max_zavernenkov@mail.ru>
2015-09-23 18:39:15 +0300

    Editing notes is working

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -23,15 +23,49 @@ export default class App extends React.Component {
                 }
             ]
         }
+
+        this.findNote = this.findNote.bind(this);
+        this.addNote = this.addNote.bind(this);
+        this.editNote = this.editNote.bind(this);
     }
     render() {
         const notes = this.state.notes;
 
         return (
             <div>
-                <Notes items={notes} />
+                <button className='add-note' onClick={this.addNote}>+</button>
+                <Notes items={notes} onEdit={this.editNote} />
             </div>
         );
     }
+    addNote() {
+        this.setState({
+            notes: this.state.notes.concat([{
+                id: uuid.v4(),
+                task: 'New Task'
+            }])
+        });
+    }
+    editNote(id, task) {
+        let notes = this.state.notes;
+        const noteIndex = this.findNote(id);
 
+        if (noteIndex < 0) {
+            return;
+        }
+
+        notes[noteIndex].task = task;
+
+        this.setState({notes});
+    }
+    findNote(id) {
+        const notes = this.state.notes;
+        const noteIndex = notes.findIndex((note) => note.id === id);
+
+        if (noteIndex < 0) {
+            console.warn('Failed to find note', notes, id);
+        }
+
+        return noteIndex;
+    }
 }
