43c3cae30d811a28a7a5a04642647741ce8fa5ba
zavernutiy <max_zavernenkov@mail.ru>
2015-09-28 11:18:21 +0300

    Using AltContainer instead of a Decorator

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,33 +1,41 @@
 //import uuid from 'node-uuid';
+import AltContainer from 'alt/AltContainer';
 import React from 'react';
 import Notes from './Notes.jsx';
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
+//import connect from '../decorators/connect';
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.storeChanged = this.storeChanged.bind(this);
-        this.state = NoteStore.getState();
-    }
-    componentDidMount() {
-        NoteStore.listen(this.storeChanged);
-    }
-    componentWillUnmount() {
-        NoteStore.unlisten(this.storeChanged);
-    }
-    storeChanged(state) {
-        this.setState(state);
-    }
+    //constructor(props) {
+    //    super(props);
+    //
+    //    this.storeChanged = this.storeChanged.bind(this);
+    //    this.state = NoteStore.getState();
+    //}
+    //componentDidMount() {
+    //    NoteStore.listen(this.storeChanged);
+    //}
+    //componentWillUnmount() {
+    //    NoteStore.unlisten(this.storeChanged);
+    //}
+    //storeChanged(state) {
+    //    this.setState(state);
+    //}
     render() {
-        const notes = this.state.notes;
+        //const notes = this.props.notes;
 
         return (
             <div>
                 <button className='add-note' onClick={this.addNote}>+</button>
-                <Notes items={notes}
-                       onEdit={this.editNote} onDelete={this.deleteNote} />
+                <AltContainer
+                    stores={[NoteStore]}
+                    inject={ {
+                        items: () => NoteStore.getState().notes
+                    } }
+                >
+                    <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+                </AltContainer>
             </div>
         );
     }
